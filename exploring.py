1d311a87078fb5556f3fe2f268d974f767e5a02c
ebon2020 <ethan.bondick@gmail.com>
2023-04-19 16:29:55 -0400

    Linked salaryScraping and dataScraping to filter out players who appear in only one database

--- exploring.py
+++ exploring.py
@@ -2,6 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-matchFrame = pd.read_csv("./data/matches/england-premier-league-matches-2017-to-2022-stats.csv")
+
 
 # add an outcome column - if 
